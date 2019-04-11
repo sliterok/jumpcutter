96083628e548736f7079b982e0541adb16de5199
sliterok <korobatov2011@yandex.ru>
2019-04-12 00:05:56 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,85 @@
+const Telegraf = require('telegraf')
+const Telegram = require('telegraf/telegram')
+const fetch = require('node-fetch')
+const fs = require('fs')
+const {promisify} = require('util')
+const mkdir = promisify(fs.mkdir)
+const exists = promisify(fs.access)
+const {exec} = require('child_process');
+const token = fs.readFileSync('token.txt')
+const telegram = new Telegram(token, {agent: null})
+const jumpCutter = new Telegraf(token, {agent: null})
+
+
+
+jumpCutter.command('start', ctx => {
+	ctx.reply('test')
+})
+
+jumpCutter.on('voice', async (ctx) => {
+	const file = (await telegram.getFile(ctx.message.voice.file_id))
+	const folder = file.file_id
+	const fileName = file.file_path.split('/').slice(-1);
+	
+	const voiceFolder = `voices/${folder}`
+	const inputFile = `${voiceFolder}/${fileName}`
+	const outputFile = `${voiceFolder}/resultVoice.oga`;
+	const tempFolder = `${voiceFolder}/TEMP`
+	
+	const folderExists = await new Promise((res, rej) => fs.access(voiceFolder, fs.constants.R_OK, err => err ? res(false) : res(true)))
+	const fileExists = await new Promise((res, rej) => fs.access(outputFile, fs.constants.R_OK, err => err ? res(false) : res(true)))
+	if(folderExists && fileExists)
+		return await new Promise(async (res, rej) => {
+			exec(`ffmpeg -i ${outputFile} 2>&1 | grep Duration | cut -d ' ' -f 4 | sed s/,//`, async (err, stdout, stderr) => {
+				let t = stdout.split('\n')[0].split('.')
+				let time = t[0].split(':').reduce((sum, val, i) => sum += parseInt(val) * 60**(2-i), parseInt(t.pop())/100)
+				await ctx.replyWithVoice({
+					source: outputFile
+				},{
+					duration: time,
+					reply_to_message_id: ctx.message.message_id
+				})
+				await exec(`rm -rf ./${voiceFolder}`)
+				res()
+			})
+		})
+	else if(folderExists && !fileExists)
+		return await new Promise(async (res, rej) => {
+			await exec(`rm -rf ./${voiceFolder}`)
+			res()
+		})
+			
+	
+	
+	await mkdir(voiceFolder, {recursive: true})
+	
+	const download = await fetch(`https://api.telegram.org/file/bot${token}/${file.file_path}`)
+	const fileStream = fs.createWriteStream(inputFile);
+	let write = await new Promise((res, erj) => {
+		download.body.pipe(fileStream);
+		download.body.on("error", e => res(e));
+		fileStream.on("finish", e => res(true));
+	});
+	
+	let cutRes = await new Promise((res, rej) => {
+		exec(`python36 jumpcutter.py --output_file ${outputFile} --input_file ${inputFile} --temp_folder ${tempFolder}`, (err, stdout, stderr) => {
+			if(err)
+				console.error(err), res({err})
+			else
+				res(stdout)
+		});
+	})
+	if(typeof cutRes === 'object')
+		return ctx.reply('Ошибка: ' + cutRes.err)
+	
+	let t = cutRes.split('\n')[0].split('.')
+	let time = t[0].split(':').reduce((sum, val, i) => sum += parseInt(val) * 60**(2-i), parseInt(t.pop())/100)
+	await ctx.replyWithVoice({
+		source: outputFile
+	},{
+		duration: time,
+		reply_to_message_id: ctx.message.message_id
+	})
+	await exec(`rm -rf ./${voiceFolder}`)
+})
+jumpCutter.startPolling()
--- jumpcutter.py
+++ jumpcutter.py
@@ -1,5 +1,4 @@
 from contextlib import closing
-from PIL import Image
 import subprocess
 from audiotsm import phasevocoder
 from audiotsm.io.wav import WavReader, WavWriter
@@ -10,28 +9,11 @@ import math
 from shutil import copyfile, rmtree
 import os
 import argparse
-from pytube import YouTube
-
-def downloadFile(url):
-    name = YouTube(url).streams.first().download()
-    newname = name.replace(' ','_')
-    os.rename(name,newname)
-    return newname
 
 def getMaxVolume(s):
     maxv = float(np.max(s))
     minv = float(np.min(s))
     return max(maxv,-minv)
-
-def copyFrame(inputFrame,outputFrame):
-    src = TEMP_FOLDER+"/frame{:06d}".format(inputFrame+1)+".jpg"
-    dst = TEMP_FOLDER+"/newFrame{:06d}".format(outputFrame+1)+".jpg"
-    if not os.path.isfile(src):
-        return False
-    copyfile(src, dst)
-    if outputFrame%20 == 19:
-        print(str(outputFrame+1)+" time-altered frames saved.")
-    return True
 
 def inputToOutputFilename(filename):
     dotIndex = filename.rfind(".")
@@ -54,11 +36,12 @@ def deletePath(s): # Dangerous! Watch out!
 
 parser = argparse.ArgumentParser(description='Modifies a video file to play at different speeds when there is sound vs. silence.')
 parser.add_argument('--input_file', type=str,  help='the video file you want modified')
+parser.add_argument('--temp_folder', type=str,  help='route to temp folder')
 parser.add_argument('--url', type=str, help='A youtube url to download and process')
 parser.add_argument('--output_file', type=str, default="", help="the output file. (optional. if not included, it'll just modify the input file name)")
-parser.add_argument('--silent_threshold', type=float, default=0.03, help="the volume amount that frames' audio needs to surpass to be consider \"sounded\". It ranges from 0 (silence) to 1 (max volume)")
+parser.add_argument('--silent_threshold', type=float, default=0.04, help="the volume amount that frames' audio needs to surpass to be consider \"sounded\". It ranges from 0 (silence) to 1 (max volume)")
 parser.add_argument('--sounded_speed', type=float, default=1.00, help="the speed that sounded (spoken) frames should be played at. Typically 1.")
-parser.add_argument('--silent_speed', type=float, default=5.00, help="the speed that silent frames should be played at. 999999 for jumpcutting.")
+parser.add_argument('--silent_speed', type=float, default=999999, help="the speed that silent frames should be played at. 999999 for jumpcutting.")
 parser.add_argument('--frame_margin', type=float, default=1, help="some silent frames adjacent to sounded frames are included to provide context. How many frames on either the side of speech should be included? That's this variable.")
 parser.add_argument('--sample_rate', type=float, default=44100, help="sample rate of the input and output videos")
 parser.add_argument('--frame_rate', type=float, default=30, help="frame rate of the input and output videos. optional... I try to find it out myself, but it doesn't always work.")
@@ -87,13 +70,14 @@ if len(args.output_file) >= 1:
 else:
     OUTPUT_FILE = inputToOutputFilename(INPUT_FILE)
 
-TEMP_FOLDER = "TEMP"
+if args.temp_folder != None:
+    TEMP_FOLDER = args.temp_folder
+else:
+	TEMP_FOLDER = "TEMP"
+	
 AUDIO_FADE_ENVELOPE_SIZE = 400 # smooth out transitiion's audio by quickly fading in/out (arbitrary magic number whatever)
     
 createPath(TEMP_FOLDER)
-
-command = "ffmpeg -i "+INPUT_FILE+" -qscale:v "+str(FRAME_QUALITY)+" "+TEMP_FOLDER+"/frame%06d.jpg -hide_banner"
-subprocess.call(command, shell=True)
 
 command = "ffmpeg -i "+INPUT_FILE+" -ab 160k -ac 2 -ar "+str(SAMPLE_RATE)+" -vn "+TEMP_FOLDER+"/audio.wav"
 
@@ -177,16 +161,6 @@ for chunk in chunks:
         outputAudioData[outputPointer:outputPointer+AUDIO_FADE_ENVELOPE_SIZE] *= mask
         outputAudioData[endPointer-AUDIO_FADE_ENVELOPE_SIZE:endPointer] *= 1-mask
 
-    startOutputFrame = int(math.ceil(outputPointer/samplesPerFrame))
-    endOutputFrame = int(math.ceil(endPointer/samplesPerFrame))
-    for outputFrame in range(startOutputFrame, endOutputFrame):
-        inputFrame = int(chunk[0]+NEW_SPEED[int(chunk[2])]*(outputFrame-startOutputFrame))
-        didItWork = copyFrame(inputFrame,outputFrame)
-        if didItWork:
-            lastExistingFrame = inputFrame
-        else:
-            copyFrame(lastExistingFrame,outputFrame)
-
     outputPointer = endPointer
 
 wavfile.write(TEMP_FOLDER+"/audioNew.wav",SAMPLE_RATE,outputAudioData)
@@ -197,8 +171,11 @@ for endGap in range(outputFrame,audioFrameCount):
     copyFrame(int(audioSampleCount/samplesPerFrame)-1,endGap)
 '''
 
-command = "ffmpeg -framerate "+str(frameRate)+" -i "+TEMP_FOLDER+"/newFrame%06d.jpg -i "+TEMP_FOLDER+"/audioNew.wav -strict -2 "+OUTPUT_FILE
+
+command = "ffmpeg -i "+TEMP_FOLDER+"/audioNew.wav -strict -2 -y "+OUTPUT_FILE
 subprocess.call(command, shell=True)
 
-deletePath(TEMP_FOLDER)
+command = "ffmpeg -i "+OUTPUT_FILE+" 2>&1 | grep Duration | cut -d ' ' -f 4 | sed s/,//"
+print(subprocess.call(command, shell=True))
 
+deletePath(TEMP_FOLDER)
